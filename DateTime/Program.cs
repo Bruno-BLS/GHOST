bb6eff471039c07741a0787648e4c64c3a5ee98f
Bruno-BLS <brunol.salata@gmail.com>
2025-07-22 15:28:43 -0300

    Add files via upload
    Data e Hora

---
+++ DateTime/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace MyApp
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            //var data = new DateTime(2020, 10, 12, 8, 23, 14);
+            var data = DateTime.Now;
+            // Y = year M = mês d= dia  h= hora  s= segundo
+
+            var formatada = string.Format("{0:dd/MM/yyyy  hh:mm:ss}", data);
+            Console.WriteLine(formatada);
+
+             
+          
+
+
+
+
+
+        }
+    
+
+
+
+
+
+
+
+}
+
+
+
+}
+
+ 
